338c29259a40ecf25e6bd0f66ff7fb5b3f3dadbc
jerome145 <jerome.lessard12@gmail.com>
2026-02-26 09:08:36 -0500

    creation de l'instance poutine pour le menu et création de Poutine.js

---
+++ src/scripts/Chef.js
@@ -0,0 +1,19 @@
+import Poutine from './Poutine.js';
+
+export default class Chef {
+  constructor(element) {
+    this.element = element;
+    this.menu = [];
+    this.container = document.querySelector('.js-container');
+    this.init();
+  }
+
+  init() {
+    const poutines = this.element.querySelectorAll('.js-poutine');
+    for (let i = 0; i < poutines.length; i++) {
+      const poutine = poutines[i];
+      const instance = new Poutine(poutine);
+      this.menu.push(instance);
+    }
+  }
+}
--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,13 +1,18 @@
 import Icons from './utils/Icons.js';
+import Chef from './Chef.js';
 
-// La ligne suivante devrait être au TOUT début du init() du Main
 class main {
   constructor() {
     this.init();
   }
 
   init() {
-    Icons.load();
+    Icons.load(); // La ligne suivante devrait être au TOUT début du init() du Main
+    const chefs = document.querySelectorAll('[data-component="Chef"]');
+    for (let i = 0; i < chefs.length; i++) {
+      const chef = chefs[i];
+      new Chef(chef);
+    }
   }
 }
 new main();
